8ae2dd25d268be9694a8401afe2f72dcbe22898a
WCram <wcram@rtc.edu>
2022-09-29 19:15:49 -0700

    Updated Notes on Class

--- Program.cs
+++ Program.cs
@@ -10,9 +10,84 @@ namespace Prog_2_F22_Lecture_4
     {
         static void Main(string[] args)
         {
-
+            Menu();
 
         } // Main
+
+        public static void Menu()
+        {
+            // Menu
+            bool isRunning = true;
+
+            do
+            {
+
+                Console.WriteLine("Renton Tech College Classes and Students");
+
+                // Menu Options
+                // 1. Display Classes
+                // 2. Add a class
+                // 3. Display Students
+                // 4. Add a student
+                Console.WriteLine("1. Display Classes");
+                Console.WriteLine("2. Add A Class");
+                Console.WriteLine("3. Display Students");
+                Console.WriteLine("4. Add a student");
+                Console.WriteLine("5. Exit Application");
+
+                Console.Write("\nEnter an option: ");
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        DisplayClasses();
+                        break;
+                    case "2":
+                        AddAClass();
+                        break;
+                    case "3":
+                        DisplayStudents();
+                        break;
+                    case "4":
+                        AddAStudent();
+                        break;
+                    case "5":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid selection.");
+                        break;
+                }
+            } while (isRunning);
+
+            // Note to us: Look up what Environment.Exit(0) does
+            Environment.Exit(0);
+
+
+        } // Menu
+
+
+        public static void DisplayClasses()
+        {
+            Console.WriteLine("Displaying Classes");
+        } // DisplayClasses
+
+        public static void AddAClass()
+        {
+            Console.WriteLine("Displaying Classes");
+        } // DisplayClasses
+
+        public static void DisplayStudents()
+        {
+            Console.WriteLine("Displaying Classes");
+        } // DisplayClasses
+
+        public static void AddAStudent()
+        {
+            Console.WriteLine("Displaying Classes");
+        } // DisplayClasses
 
     } // class
 
---
+++ SchoolClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_2_F22_Lecture_4
+{
+    internal class SchoolClass
+    {
+        // fields
+        string _className;
+        string _teacher;
+        string _students;
+
+        // Constructor
+        public SchoolClass(string className)
+        {
+            _className = className;
+        } // constructor
+
+        // Properties
+        // Give access to the users.
+        // Prevent them from doing unwanted actions
+        // Getters = Return the value in the field
+        // Setters = Change the value
+
+
+
+        // Methhods
+
+    } //
+}
